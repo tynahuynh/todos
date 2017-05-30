8534731dba14e8a1e7f943d9c9fe7056d0037ee9
Tyna Huynh <tynahuynh@gmail.com>
2017-05-30 01:24:20 +0000

    Refactoring code

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -5,10 +5,25 @@ RSpec.describe TasksController, type: :controller do
 		it "should list the tasks in the database" do
 			task1 = FactoryGirl.create(:task)
 			task2 = FactoryGirl.create(:task)
+			task1.update_attributes(title: "Something else")
 			get :index
 			expect(response).to have_http_status :success
 			response_value = ActiveSupport::JSON.decode(@response.body)
 			expect(response_value.count).to eq(2)
+			response_ids = response_value.collect do |task|
+				task["id"]
+			end
+			expect(response_ids).to eq([task1.id, task2.id])
+		end
+	end
+
+	describe "tasks#update" do
+		it "should allow tasks to be marked as done" do
+			task = FactoryGirl.create(:task, done: false)
+			put :update, id: task.id, task: { done: true }
+			expect(response).to have_http_status(:success)
+			task.reload
+			expect(task.done).to eq(true)
 		end
 	end
 end
